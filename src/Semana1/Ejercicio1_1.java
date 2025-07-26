3efc3173cf09f32c382d832dd14d1fffc3461a9b
Pachaconjettt <efrain.retana.segura@est.una.ac.cr>
2025-07-26 05:02:34 -0600

    Initial commit

---
+++ src/Semana1/Ejercicio1_1.java
@@ -0,0 +1,7 @@
+package Semana1;
+
+public class Ejercicio1_1{
+    public static void main(String[] args){
+        System.out.println("¡Hola, Mundo!");
+    }
+}
---
+++ src/Semana1/Ejercicio1_2.java
@@ -0,0 +1,12 @@
+package Semana1;
+
+public class Ejercicio1_2 {
+public static void main(String[] args){
+    int a = 8;
+    int b = 3;
+    int suma = a + b;
+
+    System.out.println(a + " + " + b + " - " + suma);
+    System.out.printf("%d + %d = %d%n", a,b, suma);
+}
+}
